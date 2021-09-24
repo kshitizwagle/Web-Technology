8c6e9694c1f111f6b91d594f31f09a87e11598af
kshitizbca076 <84304838+kshitizbca076@users.noreply.github.com>
2021-09-24 19:21:31 +0545

    Delete Lab 3 directory

--- Lab 3/QN6/DivisionByZeroException.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DivisionByZeroException extends Exception
-{
-    public function exceptionMessage()
-    {
-        $exceptionMsg = "Exception on line " . $this->getLine() . " in " . $this->getFile() . " " . $this->getMessage();
-        return $exceptionMsg;
-    }
-}
